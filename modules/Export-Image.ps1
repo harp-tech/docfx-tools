434a55106c7a367c824f406315b371ec49e54c04
glopesdev <glopesdev@users.noreply.github.com>
2024-03-03 22:29:31 +0000

    Move SVG conversion functions to separate module

--- modules/Export-Image.ps1
+++ modules/Export-Image.ps1
@@ -4,45 +4,6 @@ param(
     [string]$bootstrapperPath="..\.bonsai\Bonsai.exe"
 )
 
-function Import-Svg([string]$svgFile)
-{
-    $svgDOM = New-Object System.Xml.XmlDocument
-    $settings = New-Object System.Xml.XmlReaderSettings
-    $settings.MaxCharactersFromEntities = 0;
-    $settings.DtdProcessing = [System.Xml.DtdProcessing]::Parse
-    $reader = [System.Xml.XmlReader]::Create($svgFile, $settings)
-    try {
-        $svgDOM.Load($reader)
-    }
-    finally {
-        $reader.Close()
-    }
-    return $svgDOM
-}
-
-function Convert-Svg([string]$svgFile)
-{
-    $svgDOM = Import-Svg $svgFile
-    $namespaceURI = $svgDOM.DocumentElement.NamespaceURI
-    $nsmgr = New-Object System.Xml.XmlNamespaceManager($svgDOM.NameTable)
-    $nsmgr.AddNamespace("svg", $namespaceURI)
-
-    # set transparent background
-    $svgDOM.svg.rect.style = "fill:none;"
-
-    # set responsive text style
-    $svgStyle = $svgDOM.CreateElement("style", $namespaceURI)
-    $svgStyle.InnerText = "text { fill: #000; } @media (prefers-color-scheme: dark) { text { fill: #eee; } }"
-    [void]$svgDOM.SelectSingleNode("/svg:svg", $nsmgr).PrependChild($svgStyle)
-
-    # remove default text style from all text nodes
-    foreach ($textElement in $svgDOM.SelectNodes("//svg:text", $nsmgr)) {
-        $textElement.style = $textElement.style.replace("fill:rgb(0,0,0);", "")
-    }
-
-    $svgDOM.Save($svgFile)
-}
-
 function Export-Svg([string[]]$libPath, [string]$svgFileName, [string]$workflowFile)
 {
     $bootstrapperArgs = @()
@@ -57,6 +18,7 @@ function Export-Svg([string[]]$libPath, [string]$svgFileName, [string]$workflowF
     &$bootstrapperPath $bootstrapperArgs
 }
 
+Import-Module (Join-Path $PSScriptRoot "Export-Tools.psm1")
 $sessionPath = $ExecutionContext.SessionState.Path
 foreach ($workflowFile in Get-ChildItem (Join-Path $workflowPath "*.bonsai")) {
     $svgFileName = "$($workflowFile.BaseName).svg"
