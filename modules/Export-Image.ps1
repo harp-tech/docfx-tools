164913526b4294a9884df72812d25d9c48ff4214
glopesdev <glopesdev@users.noreply.github.com>
2024-03-03 22:30:47 +0000

    Write feedback about file export operations

--- modules/Export-Image.ps1
+++ modules/Export-Image.ps1
@@ -22,6 +22,7 @@ Import-Module (Join-Path $PSScriptRoot "Export-Tools.psm1")
 $sessionPath = $ExecutionContext.SessionState.Path
 foreach ($workflowFile in Get-ChildItem (Join-Path $workflowPath "*.bonsai")) {
     $svgFileName = "$($workflowFile.BaseName).svg"
+    Write-Host "Exporting $($svgFileName)"
     $svgFile = $sessionPath.GetUnresolvedProviderPathFromPSPath((Join-Path $workflowPath $svgFileName))
     Export-Svg $libPath $svgFileName $workflowFile
     Convert-Svg $svgFile
