c44264f7d89fc0b75949e6b6311bfa3865c41347
Luís Teixeira <micboucinha@gmail.com>
2024-05-22 15:17:28 +0100

    Allow for sub folders under workflows

--- modules/Export-Image.ps1
+++ modules/Export-Image.ps1
@@ -21,10 +21,11 @@ function Export-Svg([string[]]$libPath, [string]$svgFileName, [string]$workflowF
 
 Import-Module (Join-Path $PSScriptRoot "Export-Tools.psm1")
 $sessionPath = $ExecutionContext.SessionState.Path
-foreach ($workflowFile in Get-ChildItem -File (Join-Path $workflowPath "*.bonsai")) {
+foreach ($workflowFile in Get-ChildItem -File -Recurse (Join-Path $workflowPath "*.bonsai")) {
     $svgFileName = "$($workflowFile.BaseName).svg"
     Write-Host "Exporting $($svgFileName)"
-    $svgFile = $sessionPath.GetUnresolvedProviderPathFromPSPath((Join-Path $workflowPath $svgFileName))
+    $svgFileDirectory = Split-Path -Parent $workflowFile.FullName
+    $svgFile = $sessionPath.GetUnresolvedProviderPathFromPSPath((Join-Path $svgFileDirectory $svgFileName))
     Export-Svg $libPath $svgFileName $workflowFile
     Convert-Svg $svgFile
 }
